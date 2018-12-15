74c3033c591edc91fc94ac73e9bf2227cdf3be70
Zhang XueNan <zxn@zxnnet.top>
2018-12-15 18:45:40 +0800

    test Again

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -2,21 +2,21 @@
 
 })();*/
 
-const applicationServerPublicKey = 'BDHKPaCLbGyTeocs9pWEMR9k9dAUM1H0FcqSpdFkhvlUm813A9eeQSaBSXLh7hlNeJHzwZukKE7lH91nMpmAS3w';
+// const applicationServerPublicKey = 'BDHKPaCLbGyTeocs9pWEMR9k9dAUM1H0FcqSpdFkhvlUm813A9eeQSaBSXLh7hlNeJHzwZukKE7lH91nMpmAS3w';
 
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
+// if ('serviceWorker' in navigator && 'PushManager' in window) {
+//   console.log('Service Worker and Push is supported');
 
-  navigator.serviceWorker.register('/sw.js')
-    .then(function(swReg) {
-      console.log('Service Worker is registered', swReg);
+//   navigator.serviceWorker.register('/sw.js')
+//     .then(function(swReg) {
+//       console.log('Service Worker is registered', swReg);
 
-      swRegistration = swReg;
-    })
-    .catch(function(error) {
-      console.error('Service Worker Error', error);
-    });
-} else {
-  console.warn('Push messaging is not supported');
-  pushButton.textContent = 'Push Not Supported';
-}
+//       swRegistration = swReg;
+//     })
+//     .catch(function(error) {
+//       console.error('Service Worker Error', error);
+//     });
+// } else {
+//   console.warn('Push messaging is not supported');
+//   pushButton.textContent = 'Push Not Supported';
+// }
