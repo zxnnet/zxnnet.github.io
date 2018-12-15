c0b17223068cd3b93684ee28bf4358ed667ae59b
Zhang XueNan <zxn@zxnnet.top>
2018-12-15 19:05:03 +0800

    I give up

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Hello',
+    icon: 'assets/android-chrome-192x192.png',
+    badge: 'assets/images/logo/logo.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
