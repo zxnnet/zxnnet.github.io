1d15e6cff489ede3bce35adfdcb1fd9f7b63ba4e
Zhang XueNan <zxn@zxnnet.top>
2018-12-15 18:17:53 +0800

    Test again

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 function registerServiceWorker() {
   // register sw script in supporting browsers
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', {
+    navigator.serviceWorker.register('/sw.js', {
       scope: '/'
     }).then(() => {
       console.log('Service Worker registered successfully.');
@@ -10,28 +10,3 @@ function registerServiceWorker() {
     });
   }
 }
-
-// sw.js
-self.addEventListener('install', e => {
-  e.waitUntil(
-    // after the service worker is installed,
-    // open a new cache
-    caches.open('my-pwa-cache').then(cache => {
-      // add all URLs of resources we want to cache
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/about.html',
-        '/assets/images/logo/logo.png',
-        '/assets/css/main.scss',
-        '/assets/android-chrome-192x192.png',
-        '/assets/images/logo/logo.svg',
-        '/assets/android-chrome-512x512.png',
-        '/assets/apple-touch-icon.png',
-        '/assets/browserconfig.xml',
-        '/assets/favicon-16x16.png',
-        '/assets/favicon.ico'
-      ]);
-    })
-  );
-});
