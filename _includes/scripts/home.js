0b2344dcd16d209592cad3d61971cdd3cddb75ed
Zhang XueNan <zxn@zxnnet.top>
2018-12-15 19:58:58 +0800

    Finsally Give Up

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -4,19 +4,15 @@
 
 // const applicationServerPublicKey = 'BDHKPaCLbGyTeocs9pWEMR9k9dAUM1H0FcqSpdFkhvlUm813A9eeQSaBSXLh7hlNeJHzwZukKE7lH91nMpmAS3w';
 
-// if ('serviceWorker' in navigator && 'PushManager' in window) {
-//   console.log('Service Worker and Push is supported');
-
-//   navigator.serviceWorker.register('/sw.js')
-//     .then(function(swReg) {
-//       console.log('Service Worker is registered', swReg);
-
-//       swRegistration = swReg;
-//     })
-//     .catch(function(error) {
-//       console.error('Service Worker Error', error);
-//     });
-// } else {
-//   console.warn('Push messaging is not supported');
-//   pushButton.textContent = 'Push Not Supported';
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
